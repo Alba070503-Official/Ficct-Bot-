8191eaed46fb40006e9a437b6a54a6d02d58410b
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-02-11 22:53:56 -0400

    Update Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -1,20 +1,25 @@
-import fs from 'fs';
 import fetch from 'node-fetch';
-const sendPDF = async (conn, m, url, fileName) => {
-  const res = await fetch(url);
-  const buffer = await res.buffer();
-  const tempFilePath = `/tmp/${fileName}`;
-  fs.writeFileSync(tempFilePath, buffer);
-  await conn.sendMessage(m.chat, { document: { url: tempFilePath }, mimetype: 'application/pdf', fileName }, { quoted: m });
-  // Clean up temporary file
-  fs.unlinkSync(tempFilePath);
-};
+
 let handler = async (m, { conn }) => {
-  const pdfUrl = 'https://qu.ax/ewTsT.pdf';
-  const fileName = 'Maestro En Oferta 1/2025.pdf';
-  await sendPDF(conn, m, pdfUrl, fileName);
+  try {
+    const pdfUrl = 'https://qu.ax/ewTsT.pdf';
+    const fileName = 'Maestro_En_Oferta_1_2025.pdf';
+
+    // Enviar el PDF directamente desde la URL
+    await conn.sendMessage(m.chat, {
+      document: { url: pdfUrl },
+      mimetype: 'application/pdf',
+      fileName: fileName
+    }, { quoted: m });
+
+  } catch (e) {
+    console.error(e);
+    conn.reply(m.chat, '❗ *Error:* No se pudo enviar el documento. Inténtalo nuevamente.', m);
+  }
 };
-handler.help = ['sendpdf'];
-handler.tags = ['utility'];
+
+handler.help = ['maestro'];
+handler.tags = ['pdf'];
 handler.command = ['maestro'];
+
 export default handler;
