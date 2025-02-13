b9fa244d1bfe79e93d4e4b5dd684f183fa8ed71a
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-02-12 22:17:14 -0400

    Update Descargas•play.js

--- plugins/Descargas•play.js
+++ plugins/Descargas•play.js
@@ -13,64 +13,22 @@ https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
 https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
 */
 
-// *𓍯𓂃𓏧♡  PLAY (audio - video)*
+// *𓍯𓂃𓏧♡ YTMP4*
 
-import fetch from 'node-fetch'
-import yts from 'yt-search'
+import axios from 'axios'
 
-let handler = async (m, { conn, text, args }) => {
-if (!text)  return conn.reply(m.chat, `❀ Ingresa el nombre de lo que quieres buscar`, m)
-
+let HS = async (m, { conn, text }) => {
+if (!text)  return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
 
 try {
-let res = await search(args.join(" "))
-
-let apiAud = await fetch(`https://api.agungny.my.id/api/youtube-audio?url=${'https://youtu.be/' + res[0].videoId}`)
-let dataAud = await apiAud.json()
-let apiVid = await fetch(`https://api.agungny.my.id/api/youtube-video?url=${'https://youtu.be/' + res[0].videoId}`)
-let dataVid = await apiVid.json()
-
-
-let txt = `*◆ [ YOUTUBE - PLAY ] ◆*
-- *Titulo:* ${res[0].title}
-- *Duracion:* ${res[0].timestamp}
-- *Visitas:* ${res[0].views}
-- *Subido:* ${res[0].ago}
-
-◆────────────────◆
-
-Responde a este mensaje dependiendo lo que quieras :
-
-1 : Audio
-2 : Video`
-
-let SM = await conn.sendFile(m.chat, res[0].thumbnail, 'HasumiBotFreeCodes.jpg', txt, m)
-conn.ev.on("messages.upsert", async (upsertedMessage) => {
-let RM = upsertedMessage.messages[0];
-if (!RM.message) return
-
-const UR = RM.message.conversation || RM.message.extendedTextMessage?.text
-let UC = RM.key.remoteJid
-
-if (RM.message.extendedTextMessage?.contextInfo?.stanzaId === SM.key.id) {
-
-if (UR === '1') {
-  await conn.sendMessage(UC, { audio: { url: dataAud.result.downloadUrl }, mimetype: "audio/mpeg", caption: null }, { quoted: RM })
-} else if (UR === '2') {
-  await conn.sendMessage(m.chat, { video: { url: dataVid.result.downloadUrl }, caption: ``, mimetype: 'video/mp4', fileName: `${res[0].title}` + `.mp4`}, {quoted: m })
-} else {
-await conn.sendMessage(UC, { text: "Opcion invalida, responde con 1 *(audio)* o 2 *(video)*." }, { quoted: RM })
-}}})
-
+let api = await axios.get(`https://api.agungny.my.id/api/youtube-video?url=${text}`)
+let json = await api.data
+let { id, image, title, downloadUrl:dl_url } = json.result
+await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${title}.mp4`, mimetype: 'video/mp4', caption: `` }, { quoted: m })
 } catch (error) {
 console.error(error)
 }}
 
-handler.command = ["play"]
+HS.command = ['ytmp4', 'yta']
 
-export default handler
-
-async function search(query, options = {}) {
-  let search = await yts.search({ query, hl: "es", gl: "ES", ...options })
-  return search.videos
-}
+export default HS
